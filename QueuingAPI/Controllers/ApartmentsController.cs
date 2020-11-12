c76154bf0aed5065c6c792d7dda3532f40e35a24
Samira Abdoljavadi <saab0029@student.hv.se>
2020-11-12 16:52:01 +0100

    Create API

---
+++ QueuingAPI/Controllers/ApartmentsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QueuingAPI.Models;
+using QueuingAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QueuingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentsController : ControllerBase
+    {
+        private readonly ApartmentsService _apartmentsService;
+
+        public ApartmentsController(ApartmentsService apartmentsService)
+        {
+            _apartmentsService = apartmentsService;
+        }
+
+        // GET: api/<ApartmentsController>
+        [HttpGet]
+        public ActionResult<List<Apartments>> Get() =>
+            _apartmentsService.Get();
+
+        // GET api/<ApartmentsController>/5
+        [HttpGet("{id:length(24)}", Name = "Details")]
+        public ActionResult<Apartments> Get(string id)
+        {
+            var apartment = _apartmentsService.Get(id);
+
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return apartment;
+        }
+
+        // POST api/<ApartmentsController>
+        [HttpPost]
+        public ActionResult<Apartments> Create(Apartments apartment)
+        {
+            //apartment.id = new Random().Next();
+            _apartmentsService.Create(apartment);
+
+            return CreatedAtRoute("Details", new { id = apartment.objectId.ToString() }, apartment);
+        }
+
+        // PUT api/<ApartmentsController>/5
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Apartments apartmentIn)
+        {
+            var apartment = _apartmentsService.Get(id);
+
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            apartmentIn.objectId = id;
+            _apartmentsService.Update(id, apartmentIn);
+
+            return NoContent();
+        }
+
+
+        // DELETE api/<ApartmentsController>/5
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var apartment = _apartmentsService.Get(id);
+
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            _apartmentsService.Remove(apartment.objectId);
+
+            return NoContent();
+        }
+    }
+}
---
+++ QueuingAPI/Models/Apartments.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace QueuingAPI.Models
+{
+    public class Apartments
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string objectId { get; set; }
+
+        public int id { get; set; }
+
+        //[JsonProperty(PropertyName = "id")]
+        //public string Id { get; set; }
+
+
+        [JsonProperty(PropertyName = "Situation")]
+        public string Situation { get; set; }
+
+        [JsonProperty(PropertyName = "Street")]
+        [Required]
+        public string Street { get; set; }
+
+        [JsonProperty(PropertyName = "Area")]
+        [Display(Name = "Zone")]
+        public string Area { get; set; }
+
+        [JsonProperty(PropertyName = "City")]
+        [Required]
+        public string City { get; set; }
+
+        [JsonProperty(PropertyName = "Rooms")]
+        [Required]
+        public int Rooms { get; set; }
+
+        [JsonProperty(PropertyName = "SquareMeters")]
+        [Display(Name = "Area")]
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:#,0}")]
+        public decimal SquareMeters { get; set; }
+
+        [JsonProperty(PropertyName = "Rent")]
+        [Required]
+        public int Rent { get; set; }
+
+        [JsonProperty(PropertyName = "MoveInDate")]
+        [Display(Name = "Move in Date")]
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")] //To remove time from display
+        public DateTime MoveInDate { get; set; }
+
+        [JsonProperty(PropertyName = "Floor")]
+        [Required]
+        public int Floor { get; set; }
+
+        [JsonProperty(PropertyName = "WholeFloor")]
+        [Display(Name = "Whole Floor")]
+        public int WholeFloor { get; set; }
+
+        [JsonProperty(PropertyName = "PublishDate")]
+        [Display(Name = "Publish Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")] //To remove time from display
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        [JsonProperty(PropertyName = "Pets")]
+        public string Pets { get; set; }
+
+        [JsonProperty(PropertyName = "Smoke")]
+        public string Smoke { get; set; }
+
+        [JsonProperty(PropertyName = "LandlordComments")]
+        [Display(Name = "Landlord Comments")]
+        public string LandlordComments { get; set; }
+
+        [JsonProperty(PropertyName = "Properties")]
+        public string Properties { get; set; }
+
+        [JsonProperty(PropertyName = "RentInclude")]
+        [Display(Name = "Rent Include")]
+        public string RentInclude { get; set; }
+
+        [JsonProperty(PropertyName = "ImageUrl")]
+        [Display(Name = "Photo")]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+    }
+}
